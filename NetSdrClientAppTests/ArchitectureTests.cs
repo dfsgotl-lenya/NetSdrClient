1a7b8fc21690c45a95bdd3cdca00d1b65460b444
dfsgotl-lenya <7320765@stud.kai.edu.ua>
2025-11-02 14:12:50 +0200

    Create ArchitectureTests.cs

---
+++ NetSdrClientAppTests/ArchitectureTests.cs
@@ -0,0 +1,23 @@
+﻿using NetArchTest.Rules;
+using NUnit.Framework;
+
+namespace NetSdrClientAppTests
+{
+    public class ArchitectureTests
+    {
+        [Test]
+        public void UI_ShouldNotDependOn_EchoTspServer()
+        {
+            var assembly = typeof(NetSdrClientApp.NetSdrClient).Assembly;
+
+            var result = Types.InAssembly(assembly)
+                .That()
+                .ResideInNamespace("NetSdrClientApp")
+                .ShouldNot()
+                .HaveDependencyOn("EchoTspServer")
+                .GetResult();
+
+            Assert.That(result.IsSuccessful, Is.True, "NetSdrClientApp should not depend directly on EchoTspServer layer.");
+        }
+    }
+}
