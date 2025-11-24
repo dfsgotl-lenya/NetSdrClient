e8c8075eb860f5f41e30af487791d14d97bebaac
dfsgotl-lenya <7320765@stud.kai.edu.ua>
2025-11-24 05:25:02 +0200

    Update NetSdrMessageHelperTests.cs

--- NetSdrClientAppTests/NetSdrMessageHelperTests.cs
+++ NetSdrClientAppTests/NetSdrMessageHelperTests.cs
@@ -1,195 +1,160 @@
-using NetSdrClientApp.Messages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.PortableExecutable;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace NetSdrClientAppTests
+namespace NetSdrClientApp.Messages
 {
-    public class NetSdrMessageHelperTests
+    //TODO: analyze possible use of [StructLayout] for better performance and readability 
+    public static class NetSdrMessageHelper
     {
-        [SetUp]
-        public void Setup()
+        private const short _maxMessageLength = 8191;
+        private const short _maxDataItemMessageLength = 8194;
+        private const short _msgHeaderLength = 2; //2 byte, 16 bit
+        private const short _msgControlItemLength = 2; //2 byte, 16 bit
+        private const short _msgSequenceNumberLength = 2; //2 byte, 16 bit
+
+        public enum MsgTypes
         {
+            SetControlItem,
+            CurrentControlItem,
+            ControlItemRange,
+            Ack,
+            DataItem0,
+            DataItem1,
+            DataItem2,
+            DataItem3
         }
 
-        [Test]
-        public void GetControlItemMessageTest()
+        public enum ControlItemCodes : ushort
         {
-            //Arrange
-            var type = NetSdrMessageHelper.MsgTypes.Ack;
-            var code = NetSdrMessageHelper.ControlItemCodes.ReceiverState;
-            int parametersLength = 7500;
-
-            //Act
-            byte[] msg = NetSdrMessageHelper.GetControlItemMessage(type, code, new byte[parametersLength]);
-
-            var headerBytes = msg.Take(2);
-            var codeBytes = msg.Skip(2).Take(2);
-            var parametersBytes = msg.Skip(4);
-
-            var num = BitConverter.ToUInt16(headerBytes.ToArray());
-            var actualType = (NetSdrMessageHelper.MsgTypes)(num >> 13);
-            var actualLength = num - ((int)actualType << 13);
-            var actualCode = BitConverter.ToInt16(codeBytes.ToArray());
-
-            //Assert
-            Assert.That(headerBytes.Count(), Is.EqualTo(2));
-            Assert.That(msg.Length, Is.EqualTo(actualLength));
-            Assert.That(type, Is.EqualTo(actualType));
-
-            Assert.That(actualCode, Is.EqualTo((short)code));
-
-            Assert.That(parametersBytes.Count(), Is.EqualTo(parametersLength));
+            None = 0,
+            IQOutputDataSampleRate = 0x00B8,
+            RFFilter = 0x0044,
+            ADModes = 0x008A,
+            ReceiverState = 0x0018,
+            ReceiverFrequency = 0x0020
         }
 
-        [Test]
-        public void GetDataItemMessageTest()
+        public static byte[] GetControlItemMessage(MsgTypes type, ControlItemCodes itemCode, byte[] parameters)
         {
-            //Arrange
-            var type = NetSdrMessageHelper.MsgTypes.DataItem2;
-            int parametersLength = 7500;
-
-            //Act
-            byte[] msg = NetSdrMessageHelper.GetDataItemMessage(type, new byte[parametersLength]);
-
-            var headerBytes = msg.Take(2);
-            var parametersBytes = msg.Skip(2);
-
-            var num = BitConverter.ToUInt16(headerBytes.ToArray());
-            var actualType = (NetSdrMessageHelper.MsgTypes)(num >> 13);
-            var actualLength = num - ((int)actualType << 13);
-
-            //Assert
-            Assert.That(headerBytes.Count(), Is.EqualTo(2));
-            Assert.That(msg.Length, Is.EqualTo(actualLength));
-            Assert.That(type, Is.EqualTo(actualType));
-
-            Assert.That(parametersBytes.Count(), Is.EqualTo(parametersLength));
-        }
-        [Test]
-        public void TranslateMessage_ControlItem_Success()
-        {
-            // Arrange
-            var type = NetSdrMessageHelper.MsgTypes.Ack;
-            var code = NetSdrMessageHelper.ControlItemCodes.ReceiverFrequency;
-            byte[] parameters = { 1, 2, 3, 4 };
-
-            // Act
-            var msg = NetSdrMessageHelper.GetControlItemMessage(type, code, parameters);
-            bool success = NetSdrMessageHelper.TranslateMessage(msg, out var parsedType, out var parsedCode, out var seq, out var body);
-
-            // Assert
-            Assert.That(success, Is.True);
-            Assert.That(parsedType, Is.EqualTo(type));
-            Assert.That(parsedCode, Is.EqualTo(code));
-            Assert.That(seq, Is.EqualTo(0));
-            Assert.That(body, Is.EqualTo(parameters));
+            return GetMessage(type, itemCode, parameters);
         }
 
-        [Test]
-        public void TranslateMessage_DataItem_Success()
+        public static byte[] GetDataItemMessage(MsgTypes type, byte[] parameters)
         {
-            // Arrange
-            var type = NetSdrMessageHelper.MsgTypes.DataItem1;
-            byte[] parameters = { 9, 8, 7, 6 };
-
-            // Act
-            var msg = NetSdrMessageHelper.GetDataItemMessage(type, parameters);
-            bool success = NetSdrMessageHelper.TranslateMessage(msg, out var parsedType, out var parsedCode, out var seq, out var body);
-
-            // Assert
-            Assert.That(success, Is.True);
-            Assert.That(parsedType, Is.EqualTo(type));
-            Assert.That(parsedCode, Is.EqualTo(NetSdrMessageHelper.ControlItemCodes.None));
-            Assert.That(body.Length, Is.EqualTo(parameters.Length - 2)); // minus sequence bytes
+            return GetMessage(type, ControlItemCodes.None, parameters);
         }
 
-        [Test]
-        public void TranslateMessage_InvalidControlItemCode_ShouldFail()
+        private static byte[] GetMessage(MsgTypes type, ControlItemCodes itemCode, byte[] parameters)
         {
-            // Arrange
-            var type = NetSdrMessageHelper.MsgTypes.Ack;
-            byte[] msg = NetSdrMessageHelper.GetControlItemMessage(type, (NetSdrMessageHelper.ControlItemCodes)9999, new byte[] { 1, 2 });
-
-            // corrupt item code bytes to an undefined value
-            msg[2] = 0xFF;
-            msg[3] = 0xFF;
-
-            // Act
-            bool success = NetSdrMessageHelper.TranslateMessage(msg, out _, out _, out _, out _);
-
-            // Assert
-            Assert.That(success, Is.False);
-        }
-
-        [Test]
-        public void TranslateMessage_WrongBodyLength_ShouldFail()
-        {
-            // Arrange
-            var type = NetSdrMessageHelper.MsgTypes.Ack;
-            var msg = NetSdrMessageHelper.GetControlItemMessage(type, NetSdrMessageHelper.ControlItemCodes.RFFilter, new byte[] { 1, 2 });
-
-            // Cut last byte to mismatch length
-            msg = msg.Take(msg.Length - 1).ToArray();
-
-            // Act
-            bool success = NetSdrMessageHelper.TranslateMessage(msg, out _, out _, out _, out _);
-
-            // Assert
-            Assert.That(success, Is.False);
-        }
-
-        [Test]
-        public void GetSamples_ValidSamples_ShouldReturnIntegers()
-        {
-            // Arrange
-            ushort sampleSize = 16;
-            byte[] body = { 1, 0, 2, 0, 3, 0, 4, 0 };
-
-            // Act
-            var samples = NetSdrMessageHelper.GetSamples(sampleSize, body).ToList();
-
-            // Assert
-            Assert.That(samples.Count, Is.EqualTo(4));
-            Assert.That(samples[0], Is.EqualTo(BitConverter.ToInt32(new byte[] { 1, 0, 0, 0 })));
-        }
-
-        [Test]
-        public void GetSamples_InvalidSampleSize_ShouldThrow()
-        {
-            // Arrange + Act + Assert
-            Assert.Throws<ArgumentOutOfRangeException>(() =>
+            var itemCodeBytes = Array.Empty<byte>();
+            if (itemCode != ControlItemCodes.None)
             {
-                NetSdrMessageHelper.GetSamples(64, new byte[] { 1, 2, 3 }).ToList();
-            });
+                itemCodeBytes = BitConverter.GetBytes((ushort)itemCode);
+            }
+
+            var headerBytes = GetHeader(type, itemCodeBytes.Length + parameters.Length);
+
+            List<byte> msg = new List<byte>();
+            msg.AddRange(headerBytes);
+            msg.AddRange(itemCodeBytes);
+            msg.AddRange(parameters);
+
+            return msg.ToArray();
         }
 
-        [Test]
-        public void GetControlItemMessage_TooLong_ShouldThrow()
+        public static bool TranslateMessage(byte[] msg, out MsgTypes type, out ControlItemCodes itemCode, out ushort sequenceNumber, out byte[] body)
         {
-            // Arrange
-            var type = NetSdrMessageHelper.MsgTypes.SetControlItem;
-            var code = NetSdrMessageHelper.ControlItemCodes.RFFilter;
-            var longData = new byte[8200];
+            itemCode = ControlItemCodes.None;
+            sequenceNumber = 0;
+            bool success = true;
+            var msgEnumarable = msg as IEnumerable<byte>;
 
-            // Act + Assert
-            Assert.Throws<ArgumentException>(() =>
+            TranslateHeader(msgEnumarable.Take(_msgHeaderLength).ToArray(), out type, out int msgLength);
+            msgEnumarable = msgEnumarable.Skip(_msgHeaderLength);
+            msgLength -= _msgHeaderLength;
+
+            if (type < MsgTypes.DataItem0) // get item code
             {
-                NetSdrMessageHelper.GetControlItemMessage(type, code, longData);
-            });
+                var value = BitConverter.ToUInt16(msgEnumarable.Take(_msgControlItemLength).ToArray());
+                msgEnumarable = msgEnumarable.Skip(_msgControlItemLength);
+                msgLength -= _msgControlItemLength;
+
+                if (Enum.IsDefined(typeof(ControlItemCodes), value))
+                {
+                    itemCode = (ControlItemCodes)value;
+                }
+                else
+                {
+                    success = false;
+                }
+            }
+            else // get sequenceNumber
+            {
+                sequenceNumber = BitConverter.ToUInt16(msgEnumarable.Take(_msgSequenceNumberLength).ToArray());
+                msgEnumarable = msgEnumarable.Skip(_msgSequenceNumberLength);
+                msgLength -= _msgSequenceNumberLength;
+            }
+
+            body = msgEnumarable.ToArray();
+
+            success &= body.Length == msgLength;
+
+            return success;
         }
 
-        [Test]
-        public void GetHeader_EdgeCase_DataItem_MaxLength_ShouldWrapToZero()
+        public static IEnumerable<int> GetSamples(ushort sampleSize, byte[] body)
         {
-            // Arrange
-            var type = NetSdrMessageHelper.MsgTypes.DataItem0;
-            var msg = NetSdrMessageHelper.GetDataItemMessage(type, new byte[8192]);
+            sampleSize /= 8; //to bytes
+            if (sampleSize  > 4)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
 
-            // Act
-            bool success = NetSdrMessageHelper.TranslateMessage(msg, out var parsedType, out _, out _, out var body);
+            var bodyEnumerable = body as IEnumerable<byte>;
+            var prefixBytes = Enumerable.Range(0, 4 - sampleSize)
+                                      .Select(b => (byte)0);
 
-            // Assert
-            Assert.That(success, Is.True);
-            Assert.That(parsedType, Is.EqualTo(type));
-            Assert.That(body.Length, Is.EqualTo(8192 - 2));
+            while (bodyEnumerable.Count() >= sampleSize)
+            {
+                yield return BitConverter.ToInt32(bodyEnumerable
+                    .Take(sampleSize)
+                    .Concat(prefixBytes)
+                    .ToArray());
+                bodyEnumerable = bodyEnumerable.Skip(sampleSize);
+            }
+        }
+
+        private static byte[] GetHeader(MsgTypes type, int msgLength)
+        {
+            int lengthWithHeader = msgLength + 2;
+
+            //Data Items edge case
+            if (type >= MsgTypes.DataItem0 && lengthWithHeader == _maxDataItemMessageLength)
+            {
+                lengthWithHeader = 0;
+            }
+
+            if (msgLength < 0 || lengthWithHeader > _maxMessageLength)
+            {
+                throw new ArgumentException("Message length exceeds allowed value");
+            }
+
+            return BitConverter.GetBytes((ushort)(lengthWithHeader + ((int)type << 13)));
+        }
+
+        private static void TranslateHeader(byte[] header, out MsgTypes type, out int msgLength)
+        {
+            var num = BitConverter.ToUInt16(header.ToArray());
+            type = (MsgTypes)(num >> 13);
+            msgLength = num - ((int)type << 13);
+
+            if (type >= MsgTypes.DataItem0 && msgLength == 0)
+            {
+                msgLength = _maxDataItemMessageLength;
+            }
         }
     }
-}
